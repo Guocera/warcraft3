b2bdc02b238d523f6f3106ef7f43cac39acb04e7
Nick Yee <njsyee@gmail.com>
2016-01-12 08:38:56 -0700

    Barracks can create footmen.

--- lib/barracks.rb
+++ lib/barracks.rb
@@ -1,5 +1,5 @@
 class Barracks
-  attr_reader :gold, :food
+  attr_accessor :gold, :food
 
   def initialize
     @gold = 1000
@@ -8,5 +8,11 @@ class Barracks
 
   def can_train_footman?
     true
+  end
+
+  def train_footman
+    self.gold -= 135
+    self.food -= 2
+    Footman.new
   end
 end
